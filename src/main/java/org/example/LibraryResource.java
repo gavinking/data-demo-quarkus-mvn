7196fbf95741653ce384abb8eb187c00b449c5c1
Gavin King <gavin@hibernate.org>
2025-05-04 10:44:42 +0200

    remove annotations that seem to be unnecessary

--- src/main/java/org/example/LibraryResource.java
+++ src/main/java/org/example/LibraryResource.java
@@ -2,22 +2,17 @@ package org.example;
 
 import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
-import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.NotFoundException;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import org.jboss.resteasy.annotations.jaxrs.PathParam;
 
 import java.util.List;
 
 @Path("/books")
-@Produces(MediaType.APPLICATION_JSON)
-@Consumes(MediaType.APPLICATION_JSON)
 public class LibraryResource {
 
     @Inject
